e8b9a67e7b1ceb980596933c63d01b1a08b3a147
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-06-25 04:54:05 +0600

    work on it something,

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Hero />
+  <div class="hero">
+    <Hero />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async created() {
+    this.sale_items = await this.$content('products')
+      .where({ onSale: true })
+      .fetch()
+    this.products = await this.$content('products').fetch()
+  },
+  data() {
+    return {
+      products: null,
+      sale_items: null,
+    }
+  },
 }
 </script>
